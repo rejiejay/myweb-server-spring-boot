b32b8a22704f74095464db0e14fa62ffd141435a
rejiejay <454766952@qq.com>
2019-06-09 21:04:42 +0800

    add RejiejayWebRestfulApi

---
+++ RejiejayWebRestfulApi/src/main/java/rejiejay/Application.java
@@ -0,0 +1,13 @@
+package rejiejay;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
