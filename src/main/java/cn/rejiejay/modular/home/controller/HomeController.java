935a1c2cd5d5562c7f18f7b12a540e1505b6bea5
rejiejay <454766952@qq.com>
2019-06-10 22:05:37 +0800

    add home post Request

--- src/main/java/cn/rejiejay/modular/home/controller/HomeController.java
+++ src/main/java/cn/rejiejay/modular/home/controller/HomeController.java
@@ -3,6 +3,7 @@ package cn.rejiejay.modular.home.controller;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,9 +17,22 @@ public class HomeController {
 
     @Value("${cn.rejiejay.homeTest}")
     private String homeTest;
-
+    
+    /**
+	 * 首页Get请求
+	 * @return string
+	 */
     @RequestMapping("")
     String home() {
         return homeTest + "：Welcome to Rejiejay server side and your place in '/'.";
     }
+    
+	/**
+	 * 首页Post请求
+	 * @return string
+	 */
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String testpost() {
+        return homeTest + "：Welcome to Rejiejay server side and your place in '/'.";
+	}
 }
