fec6af5a6e5a10a040ada2eb73a8f775f6d2d52e
rejiejay <454766952@qq.com>
2019-06-10 20:26:49 +0800

    add resources

--- src/main/java/cn/rejiejay/Application.java
+++ src/main/java/cn/rejiejay/Application.java
@@ -3,17 +3,9 @@ package cn.rejiejay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
-
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
-    }
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/cn/rejiejay/modular/home/controller/HomeController.java
+++ src/main/java/cn/rejiejay/modular/home/controller/HomeController.java
@@ -1,5 +1,7 @@
 package cn.rejiejay.modular.home.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
+    @Value("${cn.rejiejay.homeTest}")
+    private String homeTest;
+
     @RequestMapping("")
     String home() {
-        return "【home】：Welcome to Rejiejay server side and your place in '/'.";
+        return homeTest + "：Welcome to Rejiejay server side and your place in '/'.";
     }
 }
