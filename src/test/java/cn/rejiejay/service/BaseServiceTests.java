2aeb15baa49e80fb3fe8eea509e21580541b7995
rejiejay <454766952@qq.com>
2019-06-29 16:49:56 +0800

    add test class

--- src/test/java/cn/rejiejay/service/BaseServiceTests.java
+++ src/test/java/cn/rejiejay/service/BaseServiceTests.java
@@ -1,4 +1,4 @@
-package cn.rejiejay;
+package cn.rejiejay.service;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,7 +10,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WebAppConfiguration // 由于是Web项目，_Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration。
-public class TmallApplicationTests {
+public class BaseServiceTests {
 	@Before
 	public void init() {
 		System.out.println("\u001B[31m -----------------开始测试----------------- \n");
--- src/test/java/cn/rejiejay/service/LoginServerTest.java
+++ src/test/java/cn/rejiejay/service/LoginServerTest.java
@@ -1,6 +1,5 @@
 package cn.rejiejay.service;
 
-import cn.rejiejay.TmallApplicationTests;
 import cn.rejiejay.service.LoginServerImpl;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,13 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
  * Created by _rejiejay Date 2018/2/2 
  * Description:测试类
  */
-public class LoginServerTest extends TmallApplicationTests {
+public class LoginServerTest extends BaseServiceTests {
 
 	@Autowired
 	private LoginServerImpl loginServerImpl;
 
+	// 测试错误密码
 	@Test
-	public void testVerifyPassword() {
+	public void testMistakePassword() {
 		Assert.assertSame("密码错误", 200, loginServerImpl.verifyPassword("1938167"));
 	}
 }
