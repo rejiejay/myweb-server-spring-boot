def26885efeb713ab200dfbdb83f5e5ff37315e9
rejiejay <454766952@qq.com>
2019-06-29 17:41:54 +0800

    add 测试用例

--- src/test/java/cn/rejiejay/controller/HomeControllerTest.java
+++ src/test/java/cn/rejiejay/controller/HomeControllerTest.java
@@ -1,18 +1,8 @@
 package cn.rejiejay.controller;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.mock.web.MockHttpSession;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -23,32 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
  * @author _rejeijay
  * @Date 2019年6月29日16:06:08
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class HomeControllerTest {
-	@Autowired
-	private WebApplicationContext wac; // 注入WebApplicationContext
-
-	private MockMvc mockMvc; // 模拟MVC对象，通过MockMvcBuilders.webAppContextSetup(this.wac).build()初始化。
-
-//  @Autowired  
-//  private MockHttpSession session;// 注入模拟的_http session  
-
-//  @Autowired  
-//  private MockHttpServletRequest request;// 注入模拟的_http request\ 
-
-	@Before // 在测试开始前初始化工作
-	public void setup() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-		
-		System.out.println("\u001B[31m -----------------开始测试----------------- \n");
-	}
-
-	@After
-	public void after() {
-		System.out.println("\u001B[31m -----------------测试结束----------------- \n \u001B[0m");
-	}
-
+public class HomeControllerTest extends BaseControllerTests {
 	@Test
 	public void testHomeGet() throws Exception {
 		MvcResult result = mockMvc.perform(get("/"))  // 简单的get 请求
---
+++ src/test/java/cn/rejiejay/controller/LoginControllerTest.java
@@ -0,0 +1,32 @@
+package cn.rejiejay.controller;
+
+import org.junit.Test;
+
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+/**
+ * 登录页测试
+ * 
+ * @author _rejeijay
+ * @Date 2019年6月29日16:06:08
+ */
+public class LoginControllerTest extends BaseControllerTests {
+
+	// 测试登录页Post请求登录
+	@Test
+	public void testHomePost() throws Exception {
+		String reqStr = "{\"password\": \"password\"}"; // 密码
+		
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.post("/login")
+					.contentType(MediaType.APPLICATION_JSON_UTF8)
+					.accept(MediaType.APPLICATION_JSON_UTF8)
+					.content(reqStr.getBytes())
+			)
+			.andReturn(); // 返回执行请求的结果
+		
+		System.out.println(result.getResponse().getContentAsString());
+	}
+}
