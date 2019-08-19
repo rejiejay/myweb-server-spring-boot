a073809189c5094c31cc26ed9845890d9649c6ba
rejiejay <454766952@qq.com>
2019-08-19 16:33:46 +0800

    add AndroidServer

---
+++ src/main/java/cn/rejiejay/service/AndroidServerImpl.java
@@ -0,0 +1,16 @@
+package cn.rejiejay.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 腾讯云对象存储 模块 实现类
+ * 
+ * @author _rejeijay
+ * @Date 2019-7-16 16:11:38
+ */
+@Service
+public class AndroidServerImpl implements AndroidServer {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+}
